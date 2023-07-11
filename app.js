6976b1e6b3e9d7eb06dc9325cdae5fdc5a6fcacc
sabarinath B <sabarinath4044.com>
2023-07-11 10:05:26 +0530

    final

--- app.js
+++ app.js
@@ -1,124 +1,3 @@
-// ////////////////////////
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-//  // Path to your database.js file
-//  let alert = require('alert');
-//  app.set('views', __dirname + '/views');
-//  app.set('view engine', 'ejs'); // Set EJS as the default view engine
-// const  connectDB= require('./config/database');
-
-// app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-// app.use(express.json()); // Parse JSON bodies// Set EJS as the default template engine
-
-// app.use(express.static('public'));
-// app.use('/js', express.static(__dirname + '/js'));
-// // const { getValuesForField } = require('./path/to/acqua');
-// // const fetchChartData = require('./chart-data');
-
-// async function getValuesForField(fieldName, callback) {
-//   try {
-//     const db = await connectDB();
-
-//     const collection = db.collection('products');
-//     const countArray = [0, 0, 0,0,0]; // Initialize the count array
-
-//     // Count documents based on value ranges
-//     countArray[0] = await collection.countDocuments({ [fieldName]: { $gt: 90, $lt: 101 } });
-//     countArray[1] = await collection.countDocuments({ [fieldName]: { $gt: 81, $lt: 91 } });
-//     countArray[2] = await collection.countDocuments({ [fieldName]: { $gte: 71, $lt: 80 } });
-//     countArray[3] = await collection.countDocuments({ [fieldName]: { $gte: 61, $lt: 70 } });
-//     countArray[4] = await collection.countDocuments({ [fieldName]: { $gte: 1, $lt: 60 } });
-
-//     // client.close();
-//     callback(countArray);
-//   } catch (error) {
-//     console.error(`Error occurred while retrieving ${fieldName} counts:`, error);
-//     callback([]);
-//   }
-// }
-
-// // Example usage:
-
-// getValuesForField('Hindi', countArray => {
-//   console.log('Count array for Hindi values:', countArray);
-//   // Perform further actions with the count array
-// });
-
-// app.get('/', async (req, res) => {
-
-//   // Example usage:
-
-//   try {
-//     const db = await connectDB();
-//     const data = await db.collection('products').find().toArray();
-
-//     if (Array.isArray(data)) {
-//       res.render('Dash', { data });
-//     } else {
-//       throw new Error('Data retrieved from the database is not an array');
-//     }
-//   } catch (error) {
-//     console.error('Error retrieving data:', error);
-//     res.status(500).send('Error retrieving data from the database');
-//   }
-// });
-
-// app.get('/enter-data', (req, res) => {
-//   res.render('enter-data', { alert: null, message: null, studentData: null });
-// });
-
-// app.post('/enter-data', async (req, res) => {
-//   try {
-//     const { _id, attendance, Math, social, English, Biology, science } = req.body;
-
-//     const db = await connectDB();
-
-//     const existingDocument = await db.collection('products').findOne({ _id: parseInt(_id) });
-
-//     if (existingDocument) {
-//       const result = await db.collection('products').updateOne(
-//         { _id: parseInt(_id) },
-//         {
-//           $set: {
-//             attendance: parseFloat(attendance),
-//             Math: parseFloat(Math),
-//             social: parseFloat(social),
-//             English: parseFloat(English),
-//             Hindi: parseFloat(English),
-//             Biology: parseFloat(Biology),
-//             science: parseFloat(science)
-//           }
-//         }
-//       );
-
-//       console.log('Updated document:', result);
-//       res.render('enter-data', { message: 'Document--- updated', alert: null, studentData: null });
-//     } else {
-//       const result = await db.collection('products').insertOne({
-//         _id: parseInt(_id),
-//         attendance: parseFloat(attendance),
-//         Math: parseFloat(Math),
-//         social: parseFloat(social),
-//         English: parseFloat(English),
-//         Hindi: parseFloat(English),
-//         Biology: parseFloat(Biology),
-//         science: parseFloat(science)
-//       });
-
-//       console.log('Inserted document:', result);
-//       res.render('enter-data', { alert: null, message: 'Document inserted successfully', studentData: null });
-//     }
-//   } catch (error) {
-//     console.error('Error inserting/updating document:', error);
-//     res.status(500).send('Error inserting/updating document in the database');
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
 
 ///////--------------------------------------------------------
 
@@ -134,6 +13,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// app.use(express.static("config"));
 app.use("/js", express.static(__dirname + "/js"));
 
 async function getValuesForFields(subjects, callback) {
@@ -192,6 +72,10 @@ app.get("/", async (req, res) => {
       _id: item._id,
       attendance: item.attendance,
     }));
+    const attendance = data.map(item => item.attendance);
+
+    
+
     console.log("attendanceData:", attendanceData);
 
     const subjects = [
@@ -204,7 +88,7 @@ app.get("/", async (req, res) => {
     ];
     getValuesForFields(subjects, counts => {
       console.log("Counts for subjects:", counts);
-      res.render("dash.ejs", { data, counts, attendanceData });
+      res.render("dash.ejs", { data, counts, attendanceData ,attendance});
       // Perform further actions with the counts object
     });
   } catch (error) {
@@ -214,7 +98,7 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/enter-data", (req, res) => {
-  res.render("enter-data", { alert: null, message: null, studentData: null });
+  res.render("enter-data");
 });
 
 app.post("/enter-data", async (req, res) => {
--- config/database.js
+++ config/database.js
@@ -40,40 +40,6 @@ async function connectDB() {
     }
   }
   
-  // async function getValuesForField(fieldName, callback) {
-  //   try {
-  //     const client = await MongoClient.connect(url);
-  //     const db = client.db(dbName);
-  
-  //     const collection = db.collection('products');
-  //     const countArray = [0, 0, 0,0,0]; // Initialize the count array
-  
-  //     // Count documents based on value ranges
-  //     countArray[0] = await collection.countDocuments({ [fieldName]: { $gt: 90, $lt: 101 } });
-  //     countArray[1] = await collection.countDocuments({ [fieldName]: { $gt: 81, $lt: 91 } });
-  //     countArray[2] = await collection.countDocuments({ [fieldName]: { $gte: 71, $lt: 80 } });
-  //     countArray[3] = await collection.countDocuments({ [fieldName]: { $gte: 61, $lt: 70 } });
-  //     countArray[4] = await collection.countDocuments({ [fieldName]: { $gte: 1, $lt: 60 } });
-  
-  
-  //     client.close();
-  //     callback(countArray);
-  //   } catch (error) {
-  //     console.error(`Error occurred while retrieving ${fieldName} counts:`, error);
-  //     callback([]);
-  //   }
-  // }
-  
-  // // Example usage:
-
-  // getValuesForField('Math', countArray => {
-  //   console.log('Count array for Hindi values:', countArray);
-  //   // Perform further actions with the count array
-  // });
-   
-  
-  
-// module.exports = getValuesForField;
 module.exports = fetchDataFromDatabase;
 module.exports = connectDB;
 
